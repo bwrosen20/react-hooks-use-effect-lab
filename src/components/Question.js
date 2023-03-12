45caba584028a813ed85790f4159e74344f20914
bwrosen20 <bwrosen20@gmail.com>
2023-03-12 09:14:51 -0400

    Final Commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+useEffect(()=>{
+  console.log("beginning")
+  
+  if (timeRemaining===0){
+    setTimeRemaining(10)
+    onAnswered(false)
+  }
+  
+  const timer=setTimeout(()=>{
+
+      setTimeRemaining(prev=>(prev-1))
+
+  },1000)
+
+  return function (){
+    console.log("I cleaned up")
+    clearTimeout(timer)
+  }
+
+
+
+},[timeRemaining,onAnswered])
+ 
+  // useEffect(()=>{
+  // const timer= setTimeout(()=>setTimeRemaining(timeRemaining>0?timeRemaining-1:timeRemaining),1000)
+ 
+  // return ()=>{
+  //   console.log("return function")
+  //   clearTimeout(timer)
+  //   handleAnswer(false)
+  // }},[timeRemaining])
+
+
+
+
+ console.log(timeRemaining)
 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
+    setTimeRemaining(10)
     onAnswered(isCorrect);
   }
 
